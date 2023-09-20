eb1da3927393771d836149dfec8eb4fa07ff3416
AndresCavalie <andrescavalie02@gmail.com>
2023-09-20 16:26:21 -0400

    Added my setup from other projecs

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -1,16 +1,35 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "secret key string"
-
-    # this tells flask of new views or urls
+    app.config['SECRET_KEY'] = 'sdlkasdjfslkjl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
     from .views import views
     from .auth import auth
 
-    # the prefix just helps with naviating files easier. If we did /auth we would have to go get it again.
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
-
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+    
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
--- website/app/views.py
+++ website/app/views.py
@@ -1,8 +1,56 @@
-from flask import Blueprint
-
-views = Blueprint("views", __name__)
-
-
-@views.route("/")  # this helps with navigation
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+views = Blueprint('views',__name__)
+from .models import Note
+from . import db
+import json
+@views.route('/', methods=['GET','POST'])
+@login_required
 def home():
-    return "<h1>Hello</h1>"  # returns this as a h1 tag (for testing)
+    if request.method == 'POST':
+        note = request.form.get('note')
+        
+        if len(note) < 1:
+            flash("Note is too short!", category="error")
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added!", category="success")
+    return render_template("home.html",user=current_user)
+
+@views.route('/home2', methods=['GET','POST'])
+@login_required
+def home2():
+    if request.method == 'POST' or request.method == 'GET':
+        return render_template("home2.html",user=current_user)
+
+
+@views.route('/favorites', methods=['GET','POST'])
+@login_required
+def favorites():
+    if request.method == 'POST' or request.method == 'GET':
+        return render_template("favorites.html",user=current_user)
+
+@views.route('/meal_plan', methods=['GET','POST'])
+@login_required
+def meal_plan():
+    if request.method == 'POST' or request.method == 'GET':
+        return render_template("meal_plan.html",user=current_user)
+
+@views.route('/discover', methods=['GET','POST'])
+@login_required
+def discover():
+    if request.method == 'POST' or request.method == 'GET':
+        return render_template("discover.html",user=current_user)
+
+@views.route('/delete-note', methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
