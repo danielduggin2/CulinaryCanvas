5161f92f117b46ed3a780a1fab8858570ad42c37
angeland001 <ange.land5673@gmail.com>
2023-10-27 13:37:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from website.app import create_app
-from flask import render_template, redirect, url_for, request, session
 from Flask import render_template, redirect, url_for, request, session
 
 app = create_app()
