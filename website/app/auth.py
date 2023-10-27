29850277b10b88a3e32f414fee6d0b89cff861d9
AndresCavalie <andrescavalie02@gmail.com>
2023-10-27 15:37:32 -0400

    Login function (w/o flash) / login deleted script

--- website/app/auth.py
+++ website/app/auth.py
@@ -10,21 +10,22 @@ auth = Blueprint("auth", __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    # if request.method == 'POST':
-    #     email = request.form.get('email')
-    #     password = request.form.get('password')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
         
-    #     user = User.query.filter_by(email=email).first()
-    #     if user:
-    #         if check_password_hash(user.password, password):
-    #             #flash('Logged in successfully!', category='success') #commented out all these flash lines for now!!!
-    #             login_user(user, remember=True)
-    #             return redirect(url_for('views.home'))
-    #         else:
-    #             #flash('Incorrect password, try again.', category='error')
-    #     else:
-    #         flash('Email does not exist', category='error')
-            
+        user = User.query.filter_by(email=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                #flash('Logged in successfully!', category='success') #commented out all these flash lines for now!!!
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                print("wrong password")
+                #flash('Incorrect password, try again.', category='error')
+        else:
+            print("email does not exist")
+            # flash('Email does not exist', category='error')
     return render_template("login.html", user=current_user)
 
 # @auth.route('/logout')
